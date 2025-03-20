eb12b183412c8edcd3574a0b48df8a3638555d2e
davidar27 <jhonatanarcos10@gmail.com>
2025-03-20 01:28:46 -0500

    REFINE Home component to integrate MainTitle and enhance layout responsiveness

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -7,40 +7,31 @@ import styles from '../../../styles';
 import { motion } from 'framer-motion';
 export const Home = () => {
 
-  const info = {
+  const info =
+  {
     title: "Jhonatan David Arcos Dominguez",
+    avatar: MiImage,
     description: "Desarrollador Full Stack con experiencia en la creación de aplicaciones web modernas y eficientes. Especializado en tecnologías frontend como React y TailwindCSS, y con sólidos conocimientos en backend utilizando Node.js, Express y bases de datos como MySQL. Apasionado por resolver problemas complejos y entregar soluciones de alta calidad, siempre enfocado en el aprendizaje continuo y las mejores prácticas de desarrollo.",
   }
 
-  const { title,description } = info;
+
 
   return (
     <section className={styles.mySection}>
-
-      <MainTitle title={title} />
-
-      <motion.button
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.95 }}
-      >
-        <motion.div
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, delay: 0.4 }}>
-          <Picture className={styles.picture} src={MiImage} />
-        </motion.div>
-      </motion.button>
-
-      <motion.div
+      <motion.div className="flex flex-col items-center gap-8"
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.5 }}>
-        <p className={styles.myDescription}>{description}</p>
+        transition={{ duration: 0.5, delay: 0.3 }}
+      >
+        <MainTitle title={info.title} />
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+        >
+          <Picture className={styles.picture} src={info.avatar} />
+        </motion.button>
+        <p className={styles.myDescription}>{info.description}</p>
       </motion.div>
-
-
-
-
     </section>
   );
 };
